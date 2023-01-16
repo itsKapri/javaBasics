6fbd20ebd4cd237f542847be36005416a2e0c9ca
नीलेश <70235074+itsKapri@users.noreply.github.com>
2023-01-17 01:53:00 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,15 @@
+//There are two types of datatypes primitive data types and non-primitive data types 
+//primitive data types are boolean , char ,short ,int ,long ,float ,double --in which value can not be null and there size are predefine
+//non-primitive data types array ,string , class, object, interface etc--->Which can be null and are size are dife at the time of use
+// https://static.javatpoint.com/images/java-data-types.png
+
+import java.util.*;    //it imports all the classes
+public class datatypes{
+  
+  public static viod main(String[] args){
+      //datatypes
+      int i=2;      //4bytes
+      double i=1;   //8bytes  and long
+      float i=1.2;   //4bytes
+      char a="A";    //2bytes
+      //bool t || f;  //1bit    
