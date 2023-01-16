1a1d25517526e9595dbd4ac5319de2b5ac3dfbd6
नीलेश <70235074+itsKapri@users.noreply.github.com>
2023-01-17 02:13:49 +0530

    Update voidfunction.java

--- voidfunction.java
+++ voidfunction.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+public class voidfunction{
+  
+  public static void sum(int a,int b){
+        System.out.print(a+b);   //sum of two number
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        sum(a,b);
+    }
+}
