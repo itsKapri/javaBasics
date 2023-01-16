8d6013e1c126cdc0bb199fa899dc77c810c2cb4f
नीलेश <70235074+itsKapri@users.noreply.github.com>
2023-01-17 02:17:36 +0530

    Update voidfunction.java

--- voidfunction.java
+++ voidfunction.java
@@ -2,8 +2,11 @@ import java.util.*;
 public class voidfunction{
   
   public static void sum(int a,int b){
-        System.out.print(a+b);   //sum of two number
-    }
+        System.out.print(a+b);                   //sum of two number
+          
+        return;    //to not important to write return in void type it doesnt effect
+  
+  }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
