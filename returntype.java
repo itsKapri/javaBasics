35e46993add094c19c274ee2303b6d834d9b2a32
नीलेश <70235074+itsKapri@users.noreply.github.com>
2023-01-17 02:21:51 +0530

    Update returntype.java

--- returntype.java
+++ returntype.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+public class returntype {
+    public static int sum(int a,int b){
+        //int k=a+b;
+        //return k;
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        int z=sum(a,b);
+        System.out.print(z);
+    }
+}
